bfccfba75805e7fcfe173b473844fc8420a141f6
German <germansanz.id@pinapp.pe>
2023-02-18 14:11:22 -0300

    thread safe singleton

--- Singleton/Singleton/src/Main.java
+++ Singleton/Singleton/src/Main.java
@@ -3,9 +3,25 @@ import singleton.Singleton;
 public class Main {
     public static void main(String[] args) {
         //If two instances shows different values, there are two Singleton instances. If not, we are OK and there is only 1 instance.
-        Singleton singleton = Singleton.getInstance("IAmTheOnlyOne");
-        Singleton singleton1 = Singleton.getInstance("NoYouAreNot");
-        System.out.println(singleton.value);
-        System.out.println(singleton1.value);
+        Thread thread1 = new Thread(new ThreadOne());
+        Thread thread2 = new Thread(new ThreadTwo());
+        thread1.start();
+        thread2.start();
+    }
+
+    static class ThreadOne implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("IAmTheOnlyOne");
+            System.out.println(singleton.value);
+        }
+    }
+
+    static class ThreadTwo implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton1 = Singleton.getInstance("NoYouAreNot");
+            System.out.println(singleton1.value);
+        }
     }
 }
--- Singleton/Singleton/src/singleton/Singleton.java
+++ Singleton/Singleton/src/singleton/Singleton.java
@@ -1,21 +1,30 @@
 package singleton;
 
 public final class Singleton {
-    private static Singleton instance;
+    // The value must be declared volatile so that double check lock would work correctly
+    private static volatile Singleton instance;
     public String value;
 
     private Singleton(String value) {
-        try {
-            Thread.sleep(1000); //Emulating slow initialization
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
         this.value = value;
     }
 
     public static Singleton getInstance(String value) {
-        if (instance == null) {
-            instance = new Singleton(value);
+        /**
+         * Using double-checked locking (DCL). It exists to prevent race condition between multiple threads that may attempt
+         * to get Singleton instance at the same time, creatin separate instances as a result.
+         *
+         * It may seem that having the 'result' variable here is pointless. There is, however, a very important ceveat when
+         * implementing double-checked locking in Java, which is solved by introducing this local variable.
+         * */
+        Singleton result = instance;
+        if(result != null){
+            return result;
+        }
+        synchronized (Singleton.class) {
+            if(instance == null) {
+                instance = new Singleton(value);
+            }
         }
         return instance;
     }
